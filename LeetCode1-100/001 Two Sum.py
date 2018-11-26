530f5e6f2f7460fd17185ca57ff81bf50a1a0002
Xiaokeai18 <im.wangpei@outlook.com>
2018-11-27 00:32:47 +0800

    update
    update code

---
+++ LeetCode1-100/001 Two Sum.py
@@ -0,0 +1,14 @@
+def twoSum(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    hash = {}
+    
+    for i in range(0,len(nums)):
+        hash[nums[i]] = i
+    for i in range(0,len(nums)):
+        if (target-nums[i]) in hash :
+            if hash.get(target-nums[i])!=i:
+                return [i,hash.get(target-nums[i])]
