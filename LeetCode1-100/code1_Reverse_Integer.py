658e6c24df08b722e0d23b4df48ecf64d6c53bc2
Xiaokeai18 <im.wangpei@outlook.com>
2018-11-28 16:00:02 +0800

    update

--- LeetCode1-100/code1_Reverse_Integer.py
+++ LeetCode1-100/code1_Reverse_Integer.py
@@ -1,14 +1,15 @@
-def twoSum(self, nums, target):
-    """
-    :type nums: List[int]
-    :type target: int
-    :rtype: List[int]
-    """
-    hash = {}
-    
-    for i in range(0,len(nums)):
-        hash[nums[i]] = i
-    for i in range(0,len(nums)):
-        if (target-nums[i]) in hash :
-            if hash.get(target-nums[i])!=i:
-                return [i,hash.get(target-nums[i])]
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hash = {}
+        
+        for i in range(0,len(nums)):
+            hash[nums[i]] = i
+        for i in range(0,len(nums)):
+            if (target-nums[i]) in hash :
+                if hash.get(target-nums[i])!=i:
+                    return [i,hash.get(target-nums[i])]
