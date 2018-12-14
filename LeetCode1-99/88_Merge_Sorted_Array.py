5ebab3b6c7675d2efe94182c3fbc3d85857128ff
Xiaokeai18 <im.wangpei@outlook.com>
2018-12-14 12:40:05 +0800

    update

---
+++ LeetCode1-99/88_Merge_Sorted_Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        if m+n: 
+            nums1[m:m+n] = nums2[:]
+            nums1.sort()
