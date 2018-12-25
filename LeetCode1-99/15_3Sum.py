41515f0fe50a4b668fa1a9f41793c23d7d3e2941
Xiaokeai18 <45148050+Xiaokeai18@users.noreply.github.com>
2018-12-25 23:56:21 +0800

    update

---
+++ LeetCode1-99/15_3Sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums_hash = {}
+        result = []
+        for num in nums:
+            nums_hash[num] = nums_hash.get(num, 0) + 1
+        if 0 in nums_hash and nums_hash[0] >= 3:
+            result.append([0, 0, 0])
+        #分别筛选出正和负的值
+        neg = list(filter(lambda x: x < 0, nums_hash))
+        pos = list(filter(lambda x: x>= 0, nums_hash))
+        for i in neg:
+            for j in pos:
+                dif = 0 - i - j
+                if dif in nums_hash:
+                    if dif in (i, j) and nums_hash[dif] >= 2:
+                        result.append([i, j, dif])
+                    if dif < i or dif > j:
+                        result.append([i, j, dif])                   
+        return result
